44bbf17b242625f96d56e7bba714a7e58f8f99d7
Facundo-Mychaliczka <facundomychaliczka@gmail.com>
2023-07-09 17:10:32 -0300

    errorRenderProductsCart

--- index.js
+++ index.js
@@ -1,51 +1,202 @@
 // ---- //
 
-// TODO ESTO ES DEL HEADER //
+  // HEADER//
 
 // BOTONES DE LOGIN
 const accountBtn = document.querySelector(".account-icon")
 const accountLog = document.querySelector(".acc-cont")
 // BOTONES DE CARRITO
 const cartBtn = document.querySelector(".cart-icon")
-const cart = document.querySelector(".cart-container")
+const cartCont = document.querySelector(".cart-container")
 // BLUR DEL FONDO AL ACTIVAR cart, ACC, O HAMBURGUESA
 const fondoBlur = document.querySelector(".fondoBlur")
 // BOTON Y MENÚ HAMBURGUESA EN SMALL DVCS
 const hambButtonSmall = document.querySelector(".navicon")
 const menuSmall = document.querySelector(".navsmall")
-
-//CONTENEDORES Y BOTONES DE LOS PRODUCTOS//
-const productContainer = document.querySelector(".products-container")
+const cart = document.querySelector(".cart")
+//------------//
+  //CONTENEDORES Y BOTONES DE LOS PRODUCTOS//
+const productsContainer = document.querySelector(".products-container")
 const showMoreBtn = document.querySelector(".showMoreBtn")
 const categoriesContainer = document.querySelector(".filter-container")
 const categoriesList = document.querySelectorAll(".category")
+const productBtn = document.querySelector(".productBtn")
+//----//
+// CONTENEDOR DE LOS PRODUCTOS DEL CART//
+const itemsCart = document.querySelector(".items-container")
+const totalCart = document.querySelector(".cart-total")
+
+// BOTONES DEL CARRITO
+const buyCartBtn = document.querySelector(".cart-buy")
+const removeCartBtn = document.querySelector(".cart-remove")
+
+// DIV DEL MENSAJE AL AGREGAR UN PRODUCTO AL CART
+const successModal=document.querySelector(".fedbackCart")
+
+
+// LOGICA DEL CARRITO (GUARDAR EN LOCAL)
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const guardarCarrito = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+////
 
 
 // FUNCIONALIDAD DE BOTON DE ACCOUNTS, JUNTO A QUE DESAPAREZCA SI HAY ALGUN OTRO MENÚ ABIERTO
 let toggleLog = () => {
     accountLog.classList.toggle("hidden")
-    if (!cart.classList.contains("hidden") || !menuSmall.classList.contains("hidden")) {
-        cart.classList.add("hidden") || menuSmall.classList.add("hidden")
+    if (!cartCont.classList.contains("hidden") || !menuSmall.classList.contains("hidden")) {
+        cartCont.classList.add("hidden") || menuSmall.classList.add("hidden")
         return
     }
     fondoBlur.classList.toggle("show-fondoBlur");
 }
 // FUNCIONALIDAD DE BOTON DE CART, JUNTO A QUE DESAPAREZCA SI HAY ALGUN OTRO MENÚ ABIERTO
 let toggleCart = () => {
-    cart.classList.toggle("hidden");
+    cartCont.classList.toggle("hidden");
     if (!accountLog.classList.contains("hidden") || !menuSmall.classList.contains("hidden"))  {
         accountLog.classList.add("hidden") || menuSmall.classList.add("hidden")
         return
     }
     fondoBlur.classList.toggle("show-fondoBlur");
-
 }
+
+//FUNCIONALIDADES DEL CART//
+
+const createCartProductTemplate = (cartProduct) => {
+    const {id, value, image, name, garantía, quantity} = cartProduct
+    return `
+            <div class="cart-item">
+                <img src=${image}>
+                    <p class="item-name">${name}</p>
+                    <p class="item-value">${value}</p>
+                    <p class="item-guarantee">${garantía}</p>
+                </div>
+                <div class="quantitys-handler">
+                    <p class="quantity-handler down" data-id=${id}>-</p>
+
+                    <p class="quantity">${quantity}</p>
+
+                    <p class="quantity-handler up" data-id=${id}>+</p>
+                     </div>
+            </div>
+    `
+}
+
+const renderCart = () => {
+    if (!carrito.lenght) {
+        itemsCart.innerHTML = `<p class="empty-cart-msg"> Actualmente, no hay productos en el carrito. </p>`;
+        return
+    }
+    itemsCart.innerHTML = carrito.map(createCartProductTemplate).join("");
+}
+
+const createProductData = (product) => {
+    const {id, name, value, image, garantía} = product
+    return {id, name, value, image, garantía}
+}
+
+const isExistingCartProduct = (productId) => {
+    return carrito.find((item) => {
+        return item.id === productId
+    })
+}
+
+const addUnitToCartProduct = (product) => {
+    carrito = carrito.map((cartProduct) => {
+        return cartProduct.id === product.id
+         ? {...cartProduct, quantity: cartProduct.quantity + 1}
+         : cartProduct
+    })
+}
+
+const showSuccesModal = (msg) => {
+    successModal.classList.add("feedbackActive")
+    successModal.textContent = msg;
+    setTimeout(() => {
+        successModal.classList.remove("feedbackActive")
+    }, 2000)
+}
+
+const createCartProduct = (product) => {
+    carrito = [
+        ...carrito,
+        {
+            ...product,
+            quantity: 1,
+        }
+    ]
+}
+
+// CAMBIAR EL DISABLED DE LOS BOTONES DEL CART
+const disableBtn = (btn) => {
+    if (carrito.length) {
+        btn.classList.add("disabled");
+    } else {
+        btn.classList.remove("disabled")
+    }
+}
+
+const updateCartState = () => {
+    //guardar carrito en el local storage
+    guardarCarrito()
+    //renderizar carrito
+    renderCart();
+    //mostrar el total del carrito
+    showTotalCart();
+    //disable de botones comprar y vaciar carrito
+    disableBtn(buyCartBtn)
+    disableBtn(removeCartBtn)
+}
+
+// FUNCIONALIDAD CE LOS BOTONES DE LOS PRODUCTOS DE AÑADIR AL CARRITO
+
+const addCartProduct = (e) => {
+    if(!e.target.classList.contains("productBtn")) {
+        return
+    }
+    const product = createProductData(e.target.dataset);
+    // si el product ya está en el carrito
+    if (isExistingCartProduct(product.id)) {
+        //se agrega una unidad en el carrito
+        addUnitToCartProduct(product)
+        //feedback
+        showSuccesModal("Se agregó una unidad al carrito.")
+    }else {
+        //si el product no está en en carrito
+         //se crea el nuevo producto en el array
+         createCartProduct(product);
+        //feedback
+        showSuccesModal("El producto ha sido añadido con éxito.")
+    }
+    
+    //actualizamos data del carrito
+    updateCartState();
+}
+
+
+// TOTAL DE LA COMPRA DEL CARRITO 
+const getCartTotal = () => {
+    return carrito.reduce ((acc, val) => {
+        return acc + Number(val.value) * Number(val.quantity);
+    },0)
+}
+
+const showTotalCart = () => {
+    totalCart.innerHTML = `TOTAL : ${getCartTotal()} U$D`
+}
+
+
+//------//
+
+
 // FUNCIONALIDAD DE MENU HAMBURGUESA EN SMALL DEVICES, JUNTO A QUE DESAPAREZCA SI HAY ALGUN OTRO MENÚ ABIERTO
 let toggleMenu = () => {
     menuSmall.classList.toggle("hidden")
 
-    if (!accountLog.classList.contains("hidden") || !cart.classList.contains("hidden")) {
-        accountLog.classList.add("hidden") || cart.classList.add("hidden")
+    if (!accountLog.classList.contains("hidden") || !cartCont.classList.contains("hidden")) {
+        accountLog.classList.add("hidden") || cartCont.classList.add("hidden")
         return
     }
     fondoBlur.classList.toggle("show-fondoBlur");
@@ -60,7 +211,7 @@ const toggleBgDisplay = () => {
 // FUNCIONALIDAD AL HACER CLICK EN EL FONDO BLUR QUE DESAPAREZCA LO QUE ESTÁ ABIERTO
 let disableNav = () => {
     accountLog.classList.add("hidden");
-    cart.classList.add("hidden");
+    cartCont.classList.add("hidden");
     menuSmall.classList.add("hidden")
     fondoBlur.classList.remove("show-fondoBlur")
 }
@@ -79,6 +230,7 @@ const createProductTemplate = (product) => {
                 <p>${value} USD</p>
                 <p>Garantía de ${garantía}</p>
                 <button
+                class="productBtn"
                 data-id= "${id}",
                 data-name= "${name}",
                 data-value= "${value}",
@@ -92,7 +244,7 @@ const createProductTemplate = (product) => {
 // EL MAP, VA POR CADA UNO DE ESOS OBJETOS (productos), Y RECIBE EL CALLBACK DE LA FUNCION DE ARRIBA//
 
 const renderProducts = (productList) => {
-    productContainer.innerHTML += productList.map(createProductTemplate).join("")
+    productsContainer.innerHTML += productList.map(createProductTemplate).join("")
 }
 
 // FUNCION PARA BOTON DE VER MÁS//
@@ -181,7 +333,6 @@ const aplicarFiltro = ({target}) => {
 }
 
 
-
 let init = () => {
     renderProducts(appState.products[appState.currentProductIndex]);
     accountBtn.addEventListener("click", toggleLog);
@@ -191,6 +342,11 @@ let init = () => {
     showMoreBtn.addEventListener("click", showMoreProducts);
     categoriesContainer.addEventListener("click", aplicarFiltro)
     menuSmall.addEventListener("click", toggleBgDisplay)
+    document.addEventListener("DOMContentLoaded", renderCart)
+    document.addEventListener("DOMContentLoaded", showTotalCart)
+    productsContainer.addEventListener("click", addCartProduct)
+    disableBtn(buyCartBtn)
+    disableBtn(removeCartBtn)
 }
 
 init ();
